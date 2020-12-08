3f5b65a61c3f5d359b79b5aaa9c109425cc5f878
Leandro <leandroacebog@gmail.com>
2020-12-08 12:11:31 +0100

    probando el uso de pdf_viewer

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_document_picker/flutter_document_picker.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -8,6 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String _localPath =
+      '/data/user/0/com.example.pdf_viewer/cache/un_plan_para_tres_docx.pdf';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,22 +20,41 @@ class _HomePageState extends State<HomePage> {
         title: Text('PDF Viewer'),
         centerTitle: true,
       ),
-      body: Container(
-          child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: null,
-              child: Text('Open PDF'),
+      body: Stack(
+        children: [
+          Container(
+            child: PDFView(
+              filePath: _localPath,
+              nightMode: true,
             ),
-            RaisedButton(
-              onPressed: null,
-              child: Text('Open PDF'),
-            ),
-          ],
-        ),
-      )),
+          ),
+        ],
+      ),
+      // body: Container(
+      //     child: Center(
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     children: <Widget>[
+      //       RaisedButton(
+      //         onPressed: _abrirPdfLocal,
+      //         child: Text('Open local PDF'),
+      //       ),
+      //       RaisedButton(
+      //         onPressed: null,
+      //         child: Text('Open PDF'),
+      //       ),
+      //     ],
+      //   ),
+      // )),
     );
+  }
+
+  void _abrirPdfLocal() async {
+    print(' HolaaaaaaaaaaaaaaaWWWWWWWW');
+    //Without parameters:
+    final _localPath = await FlutterDocumentPicker.openDocument();
+
+    print(_localPath + ' Holaaaaaaaaaaaaaaa');
+    setState(() {});
   }
 }
